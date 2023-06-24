61e4b8716dd6e071d58da6de4f5122e24848427d
scoperunner <spifmo37@gmail.com>
2023-06-24 02:26:52 +0200

    Button is now added to add-Ins tab and working

--- App.cs
+++ App.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Reflection;
 using System.Windows.Media.Imaging;
 using System.IO;
+using Adwin = Autodesk.Windows;
 #endregion
 
 namespace RevitAddinCRH
@@ -18,18 +19,22 @@ namespace RevitAddinCRH
     {
         public Result OnStartup(UIControlledApplication app)
         {
-            var panel = CreateRibbonPanel(app);
-            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            var AddinPanel = CreateRibbonPanel(Tab.AddIns, app);
+            
+            AttachOffSetButton(AddinPanel);
+            return Result.Succeeded;
+        }
 
+        private static void AttachOffSetButton(RibbonPanel panel)
+        {
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
             if (panel.AddItem(new PushButtonData("AutoTopOffsetButton", "Auto Top-Offset", thisAssemblyPath, "RevitAddinCRH.Command")) is PushButton button)
             {
                 button.ToolTip = "This Button excutes the task given by CRH Concrete. "; //Tooltip is a bit of a mouthfull since I can't see the function have a practical proffesional purpose. Since it is too unspecific.
 
                 Uri uri = new Uri(Path.Combine(Path.GetDirectoryName(thisAssemblyPath), "Resources", "icon.ico"));
                 BitmapImage iconBitmap = new BitmapImage(uri);
-                button.LargeImage = iconBitmap;
             }
-            return Result.Succeeded;
         }
 
         public Result OnShutdown(UIControlledApplication a)
@@ -37,22 +42,25 @@ namespace RevitAddinCRH
             return Result.Succeeded;
         }
 
-        public RibbonPanel CreateRibbonPanel(UIControlledApplication app) 
+        /// <summary>
+        /// Creates a RibbonPanel in given Tab.
+        /// This method Does not create a tab. it purely create and adds a panel to a existing tab.
+        /// </summary>
+        /// <returns>The RibbonPanel Created</returns>
+        private RibbonPanel CreateRibbonPanel(string TabName, UIControlledApplication app) 
         {
-            string tabName = "AutoOffsetterTab";
-            RibbonPanel ribbonPanel = null;
-
-            try { app.CreateRibbonTab(tabName); } catch(Exception ex) { Debug.WriteLine(ex.Message); }
-
-            try { app.CreateRibbonPanel(tabName, "OffsetterPanel"); } catch (Exception ex) { Debug.WriteLine(ex.Message); }
-
-            List<RibbonPanel> panels = app.GetRibbonPanels(tabName);
-            foreach (RibbonPanel panel in panels.Where(p => p.Name == "OffsetterPanel")) 
-            {
-                ribbonPanel = panel;
-            }
-
-            return ribbonPanel;
+            try { return  app.CreateRibbonPanel(TabName, "OffsetterPanel"); } catch (Exception ex) { Debug.WriteLine(ex.Message); }
+            return null;
+        }
+        /// <summary>
+        /// Creates a RibbonPanel in given Tab.
+        /// This method Does not create a tab. it purely create and adds a panel to a existing tab.
+        /// </summary>
+        /// <returns>The RibbonPanel Created</returns>
+        private RibbonPanel CreateRibbonPanel(Tab TabName, UIControlledApplication app)
+        {
+            try { return app.CreateRibbonPanel(TabName, "OffsetterPanel"); } catch (Exception ex) { Debug.WriteLine(ex.Message); }
+            return null;
         }
     }
 }
